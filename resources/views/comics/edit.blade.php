c6b86e7da7c18481faca0113dd51b32f15a1fa26
Nicholas <romitinicholas@gmail.com>
2022-02-10 17:14:28 +0100

    crud senza stile

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,34 @@
+<form action="{{route('comics.update', $comic->id)}}" method="POST">
+    @csrf
+    @method("PUT")
+
+    <div class="form-group">
+        <label for="title">Titolo</label>
+        <input type="text" class="form-control" name="title" id="title" placeholder="Inserisci il titolo del fumetto" value="{{$comic->title}}">
+    </div>
+    <div class="form-group">
+        <label for="description">Descrizione</label>
+        <textarea class="form-control" name="description" id="description" rows="8" placeholder="Inserisci la descrizione del fumetto">{{$comic->description}}</textarea>
+    </div>
+    <div class="form-group">
+        <label for="thumb">Immagine</label>
+        <input type="text" class="form-control" name="thumb" id="thumb" placeholder="Inserisci l'immagine del fumetto" value="{{$comic->thumb}}">
+    </div>
+    <div class="form-group">
+        <label for="price">Prezzo</label>
+        <input type="text" class="form-control" name="price" id="price" placeholder="Inserisci il prezzo del fumetto" value="{{$comic->price}}">
+    </div>
+    <div class="form-group">
+        <label for="series">Serie</label>
+        <input type="text" class="form-control" name="series" id="series" placeholder="Inserisci la serie del fumetto" value="{{$comic->series}}">
+    </div>
+    <div class="form-group">
+        <label for="sale_date">Anno di pubblicazione</label>
+        <input type="text" class="form-control" name="sale_date" id="sale_date" placeholder="Inserisci l'anno di pubblicazione del fumetto" value="{{$comic->sale_date}}">
+    </div>
+    <div class="form-group">
+        <label for="type">Tipo</label>
+        <input type="text" class="form-control" name="type" id="type" placeholder="Inserisci il tipo di fumetto" value="{{$comic->type}}">
+    </div>
+    <button type="submit">Modifica</button>
+</form>
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,18 @@
-<ul>
-    @foreach ($comics as $comic)
-        <li>{{$comic['title']}} <a href="{{ route('comics.show', $comic->id) }}"><button>Visualizza</button></a></li>
-    @endforeach
-</ul>
+@extends('layouts.base')
+
+@section('pageContentTitle')
+    Comics List
+@endsection
+
+@section('pageContent')
+<a href="{{ route('comics.create') }}"><button>Crea Nuovo</button></a>
+    <ul>
+        @foreach ($comics as $comic)
+            <li>{{$comic['title']}} <a href="{{ route('comics.show', $comic->id) }}"><button>Visualizza</button></a><a href="{{ route('comics.edit', $comic->id) }}"><button>Modifica</button></a><form action="{{ route('comics.destroy', $comic->id)}}" method="POST">
+            @csrf
+            @method("DELETE")
+            <button type="submit">Elimina</button>
+            </form></li>
+        @endforeach
+    </ul>
+@endsection
